b04dc2e5403e61215c29c38078765898704760c8
Amit Kumar saini <amitkumarsaini@GS0618.local>
2024-06-15 20:07:56 +0530

    added stylelint and postcss-nested

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import styles from './App.module.css';
+
 function App() {
-  return <h1 className="text-md m-1 p-1 font-bold underline">Hello world!</h1>;
+  return <h1 className={`text-md ${styles.custom} m-1 p-1 font-bold underline`}>Hello world!</h1>;
 }
 
 export default App;
